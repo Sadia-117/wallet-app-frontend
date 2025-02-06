d5881425f47a79a658fa6317e3d395d646231b63
henry <henry@kali.henry>
2025-02-07 03:13:07 +0600

    onbording1 is updated

---
+++ src/views/Onboarding2.vue
@@ -0,0 +1,25 @@
+<template>
+    <div>
+      <!-- Background Pattern -->
+      <img class="h-[590px] w-full" src="../assets//Images/Pattern.png" alt="Pattern">
+  
+      <!-- Content -->
+      <div class="p-4 ml-1">
+        <h1 class="text-4xl font-bold">
+          Track and Save your<br>
+          <span class="mt-3 mb-4"> Money Easily </span>.
+        </h1>
+        <p class="text-[#B9B9B9]">
+          Carry your favourite cards on your device<br>
+          so you can control your money usage.
+        </p>
+  
+        <!-- Steps Indicator and Button -->
+        <div class="mt-5 flex">
+          <img class="ml-2 w-[3.9rem] h-[1rem] mt-10" src="/Images/Indicator Step 2.png" alt="Step Indicator">
+          <img class="ml-[270px]" src="/Images/Button.png" alt="Next Button">
+        </div>
+      </div>
+    </div>
+  </template>
+  
